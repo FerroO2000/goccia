bf063161f7880db25b497069455e8c66a286701e
FerroO2000 <omar.ferroomar@gmail.com>
2026-03-10 18:02:12 +0100

    fix: import types

--- connector/types.go
+++ connector/types.go
@@ -11,3 +11,6 @@ type msgWrap[T msgBody] = message.Message[T]
 
 // RingBuffer is a lock-free spsc generic ring buffer.
 type RingBuffer[T any] = rb.RingBuffer[T]
+
+// MessageConnector is a utility type alias for a generic message connector.
+type MessageConnector[T msgBody] = Connector[*msgWrap[T]]
