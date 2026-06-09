23d0a13517ab3fe972fb0faddcb32c0f79103a90
Omar Ferro <omar.ferroomar@gmail.com>
2026-06-09 16:54:15 +0200

    chore: generic fan-in ingress stage

--- ingress/generic.go
+++ ingress/generic.go
@@ -68,6 +68,25 @@ func (ghb *GenericHandlerBase[Out]) SetTelemetry(tel *telemetry.Telemetry) {
 	ghb.Telemetry = tel
 }
 
+type GenericHandlerFanIn[T any, Out msgBody] interface {
+	// Init method is called once when the stage is initialized.
+	Init(ctx context.Context) error
+
+	HandleSourceContextDone()
+	HandleSource(ctx context.Context) (T, error)
+
+	HandleContextDone()
+	Handle(ctx context.Context, data T) (*msg[Out], error)
+
+	// Close is called once when the stage is closed.
+	Close()
+
+	// SetTelemetry sets the telemetry for the custom handler.
+	// It can be used to add traces, logs, and metrics to the
+	// user defined handler.
+	SetTelemetry(tel *telemetry.Telemetry)
+}
+
 // ─── Environment ────────────────────────────────────────────────────────────|
 
 type genericEnv[Out msgBody] struct {
@@ -104,6 +123,46 @@ func (ge *genericEnv[Out]) Close(ctx context.Context) {
 }
 
 func (ge *genericEnv[Out]) initConfig() {
+	ge.stopOnError = ge.Config.StopOnError
+}
+
+type genericEnvFanIn[T any, Out msgBody] struct {
+	*env.BaseEnv[*GenericConfig, *metrics.EmptyMetrics]
+
+	handler GenericHandlerFanIn[T, Out]
+
+	stopOnError bool
+}
+
+func newGenericEnvFanIn[T any, Out msgBody](
+	config *GenericConfig, handler GenericHandlerFanIn[T, Out],
+) *genericEnvFanIn[T, Out] {
+
+	return &genericEnvFanIn[T, Out]{
+		BaseEnv: env.NewProcessorEnv(config, metrics.NewEmptyMetrics()),
+
+		handler: handler,
+	}
+}
+
+func (ge *genericEnvFanIn[T, Out]) Init(ctx context.Context) error {
+	if err := ge.BaseEnv.Init(ctx); err != nil {
+		return err
+	}
+
+	ge.initConfig()
+
+	ge.handler.SetTelemetry(ge.Tel)
+	return ge.handler.Init(ctx)
+}
+
+func (ge *genericEnvFanIn[T, Out]) Close(ctx context.Context) {
+	ge.BaseEnv.Close(ctx)
+
+	ge.handler.Close()
+}
+
+func (ge *genericEnvFanIn[T, Out]) initConfig() {
 	ge.stopOnError = ge.Config.StopOnError
 }
 
@@ -158,6 +217,89 @@ func (gr *genericRunner[Out]) Run(ctx context.Context) {
 	}
 }
 
+type genericRunnerFanIn[T any, Out msgBody] struct {
+	*runnerFanInBase[*genericEnvFanIn[T, Out], Out]
+}
+
+func newGenericRunnerFanIn[T any, Out msgBody](outConnector msgConn[Out]) *genericRunnerFanIn[T, Out] {
+	return &genericRunnerFanIn[T, Out]{
+		runnerFanInBase: newRunnerFanInBase[*genericEnvFanIn[T, Out]](outConnector),
+	}
+}
+
+func (gr *genericRunnerFanIn[T, Out]) Init(ctx context.Context) error {
+	gr.initFanIn(512)
+	return nil
+}
+
+func (gr *genericRunnerFanIn[T, Out]) Run(ctx context.Context) {
+	defer gr.drainAndNotifyRunDone()
+
+	go gr.runOutputBridge(context.WithoutCancel(ctx))
+
+	go func() {
+		<-ctx.Done()
+		gr.env.handler.HandleSourceContextDone()
+	}()
+
+	for {
+		data, err := gr.env.handler.HandleSource(ctx)
+		if err != nil {
+			if ctx.Err() != nil {
+				return
+			}
+
+			if gr.env.stopOnError {
+				gr.env.Tel.LogError("failed to handle message", err)
+				return
+			}
+
+			gr.env.Tel.LogWarn("failed to handle message", err)
+			continue
+		}
+
+		gr.addWork()
+		go gr.handleData(ctx, data)
+	}
+}
+
+func (gr *genericRunnerFanIn[T, Out]) handleData(ctx context.Context, data T) {
+	defer gr.notifyWorkDone()
+
+	done := make(chan struct{})
+	defer close(done)
+
+	go func() {
+		select {
+		case <-ctx.Done():
+			gr.env.handler.HandleContextDone()
+		case <-done:
+		}
+	}()
+
+	for {
+		msgOut, err := gr.env.handler.Handle(ctx, data)
+		if err != nil {
+			if ctx.Err() != nil {
+				return
+			}
+
+			if gr.env.stopOnError {
+				gr.env.Tel.LogError("failed to handle message", err)
+				return
+			}
+
+			gr.env.Tel.LogWarn("failed to handle message", err)
+			continue
+		}
+
+		if err := gr.outConnector.Write(msgOut); err != nil {
+			msgOut.Destroy()
+			gr.env.Tel.LogError("failed to write message to output connector", err)
+		}
+	}
+}
+
 // ─── Stage ──────────────────────────────────────────────────────────────────|
 
 type GenericStage[Out msgBody] struct {
@@ -171,6 +313,21 @@ func NewGenericStage[Out msgBody](
 	return &GenericStage[Out]{
 		IngressStage: stage.NewIngressStageFromRunner[Out](
 			cfg.Name, newGenericEnv(cfg, handler), newGenericRunner(outConnector),
+		),
+	}
+}
+
+type GenericStageFanIn[T any, Out msgBody] struct {
+	*stage.IngressStage[Out, *genericEnvFanIn[T, Out]]
+}
+
+func NewGenericStageFanIn[T any, Out msgBody](
+	handler GenericHandlerFanIn[T, Out], outConnector msgConn[Out], cfg *GenericConfig,
+) *GenericStageFanIn[T, Out] {
+
+	return &GenericStageFanIn[T, Out]{
+		IngressStage: stage.NewIngressStageFromRunner[Out](
+			cfg.Name, newGenericEnvFanIn(cfg, handler), newGenericRunnerFanIn[T](outConnector),
 		),
 	}
 }
